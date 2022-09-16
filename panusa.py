2584b7822dbc1f6a0133d79ad31c73312911f54c
panusav1 <113702543+panusav1@users.noreply.github.com>
2022-09-16 21:22:15 +0700

    Update panusa.py

--- panusa.py
+++ panusa.py
@@ -32,7 +32,7 @@ acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=
 "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
 "Accept-Language: en-US,en;q=0.5\r\n"]
 
-ip = str(input('[+] Target: '))
+ip = str(input('[+] Nhập ip: '))
 port = int(input('[+] Port: '))
 pack = int(input('[+] Packet/s: '))
 thread = int(input('[+] Threads: '))
